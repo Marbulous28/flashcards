61893a64b5a039163ba1f0e165116894b9478894
Peter Beach and Ryan Harvey <peter@>
2016-03-22 14:06:35 -0700

    Added Definitions.  Added jquery functionality.

---
+++ js/myjs.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+    $(".clickable1").click(function() {
+    $(".text1").toggle();
+  });
+    $(".clickable2").click(function() {
+    $(".text2").toggle();
+  });
+    $(".clickable3").click(function() {
+    $(".text3").toggle();
+  });
+    $(".clickable4").click(function() {
+    $(".text4").toggle();
+  });
+    $(".clickable5").click(function() {
+      $(".text5").toggle();
+  });
+    $(".clickable6").click(function() {
+      $(".text6").toggle();
+  });
+    $(".clickable7").click(function() {
+      $(".text7").toggle();
+  });
+    $(".clickable8").click(function() {
+      $(".text8").toggle();
+  });
+    $(".clickable9").click(function() {
+      $(".text9").toggle();
+  });
+    $(".clickable10").click(function() {
+      $(".text10").toggle();
+  });
+    $(".clickable11").click(function() {
+      $(".text11").toggle();
+  });
+    $(".clickable12").click(function() {
+      $(".text12").toggle();
+  });
+    $(".clickable13").click(function() {
+      $(".text13").toggle();
+  });
+    $(".clickable14").click(function() {
+      $(".text14").toggle();
+  });
+    $(".clickable15").click(function() {
+      $(".text15").toggle();
+  });
+    $(".clickable16").click(function() {
+      $(".text16").toggle();
+  });
+    $(".clickable17").click(function() {
+      $(".text17").toggle();
+  });
+    $(".clickable18").click(function() {
+      $(".text18").toggle();
+  });
+    $(".clickable19").click(function() {
+      $(".text19").toggle();
+
+});
+});
